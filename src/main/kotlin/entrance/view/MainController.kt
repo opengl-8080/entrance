bc5bba110368f100a16dcf5572c2c2e9b9dd077b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-01-03 00:05:02 +0900

    doma2 が入れられないか挑戦中...

--- src/main/kotlin/entrance/view/MainController.kt
+++ src/main/kotlin/entrance/view/MainController.kt
@@ -1,6 +1,8 @@
 package entrance.view
 
+import entrance.db.ItemDao
 import javafx.fxml.Initializable
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 import java.net.URL
@@ -8,8 +10,12 @@ import java.util.ResourceBundle
 
 @Component
 class MainController : Initializable {
+    @Autowired
+    lateinit var dao: ItemDao
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         println("Hello")
+        var all = this.dao.selectAll()
+        println("all=$all")
     }
 }
