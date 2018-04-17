90a9d63b26a3017f0f2593a21344b1c7c4fa195c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-04-17 22:20:35 +0900

    タグ選択部分を別fxml化　その第一歩

---
+++ view/src/main/kotlin/entrance/view/javafx/control/TagSelectController.kt
@@ -0,0 +1,84 @@
+package entrance.view.javafx.control
+
+import entrance.domain.tag.Tag
+import entrance.domain.tag.TagRepository
+import entrance.view.javafx.util.FXPrototypeController
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.ListView
+import javafx.scene.control.TextField
+import javafx.scene.layout.FlowPane
+import java.net.URL
+import java.util.*
+
+@FXPrototypeController
+class TagSelectController(
+    private val tagRepository: TagRepository
+): Initializable {
+
+    @FXML
+    lateinit var tagFilterTextField: TextField
+    @FXML
+    lateinit var tagsFlowPane: FlowPane
+    @FXML
+    lateinit var selectedTagsListView: ListView<Tag>
+
+    private val tagViewList: ObservableList<TagView> = FXCollections.observableArrayList()
+    val selectedTagList: ObservableList<Tag> = FXCollections.observableArrayList<Tag>()
+    
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        reloadTags()
+
+        tagFilterTextField.textProperty().addListener { _, _, text ->
+            tagsFlowPane.children
+                    .map { child -> child as TagView }
+                    .forEach { tagView -> tagView.controlVisibility(text) }
+        }
+
+        selectedTagsListView.cellFactory = TagListCellFactory()
+        selectedTagsListView.items = selectedTagList
+    }
+
+    fun isNotSelected(): Boolean = selectedTagList.isEmpty()
+
+    fun selectAll(tagSet: Set<Tag>) {
+        tagViewList.filter { tagSet.contains(it.tag) }.forEach {
+            it.isSelected = true
+        }
+    }
+    
+    @FXML
+    fun reloadTags() {
+        tagsFlowPane.children.clear()
+        tagViewList.clear()
+        selectedTagList.clear()
+
+        tagRepository.findAll().tags.forEach { tag ->
+            val tagView = TagView(tag)
+            tagViewList += tagView
+            tagsFlowPane.children += tagView
+
+            tagView.selectedProperty().addListener { _, _, selected ->
+                if (selected) {
+                    selectedTagList += tag
+                } else {
+                    selectedTagList -= tag
+                }
+            }
+        }
+    }
+    
+    @FXML
+    fun clearTagSelect() {
+        tagViewList.filter { it.isSelected }.forEach { it.isSelected = false }
+        tagFilterTextField.text = ""
+    }
+    
+    @FXML
+    fun deselectTag() {
+        val tag = selectedTagsListView.selectionModel.selectedItem
+        tagViewList.filter { it.tag == tag }.forEach { it.isSelected = false }
+    }
+}
--- view/src/main/kotlin/entrance/view/javafx/window/MainController.kt
+++ view/src/main/kotlin/entrance/view/javafx/window/MainController.kt
@@ -3,10 +3,7 @@ package entrance.view.javafx.window
 import entrance.application.deletion.DeleteImageService
 import entrance.domain.viewer.StoredImage
 import entrance.domain.viewer.StoredImageRepository
-import entrance.domain.tag.Tag
-import entrance.domain.tag.TagRepository
-import entrance.view.javafx.control.TagListCellFactory
-import entrance.view.javafx.control.TagSelectionView
+import entrance.view.javafx.control.TagSelectController
 import entrance.view.javafx.control.ThumbnailsView
 import entrance.view.javafx.util.Dialog
 import entrance.view.javafx.window.categorization.CategorizationWindow
@@ -14,9 +11,7 @@ import entrance.view.javafx.window.tag.TagMaintenanceWindow
 import entrance.view.javafx.window.viewer.SingleImageViewerWindow
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
-import javafx.scene.control.ListView
 import javafx.scene.control.MenuItem
-import javafx.scene.control.TextField
 import javafx.scene.layout.FlowPane
 import javafx.stage.Stage
 import org.springframework.stereotype.Component
@@ -27,34 +22,24 @@ import java.util.*
 class MainController(
         private val tagMaintenanceWindow: TagMaintenanceWindow,
         private val categorizationWindow: CategorizationWindow,
-        private val tagRepository: TagRepository,
         private val storedImageRepository: StoredImageRepository,
         private val singleImageViewerWindow: SingleImageViewerWindow,
         private val deleteImageService: DeleteImageService
 ) : Initializable {
+    @FXML
+    lateinit var tagSelectController: TagSelectController
     
     lateinit internal var primaryStage: Stage
     
-    @FXML
-    lateinit var tagFilterTextField: TextField
-    @FXML
-    lateinit var tagsFlowPane: FlowPane
-    @FXML
-    lateinit var selectedTagsListView: ListView<Tag>
     @FXML
     lateinit var thumbnailsPane: FlowPane
     @FXML
     lateinit var openImageMenuItem: MenuItem
 
     lateinit var thumbnailsView: ThumbnailsView<StoredImage>
-    lateinit var tagSelectionView: TagSelectionView
     
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         thumbnailsView = ThumbnailsView(thumbnailsPane)
-        tagSelectionView = TagSelectionView(tagRepository, tagsFlowPane, tagFilterTextField, selectedTagsListView)
-        
-        selectedTagsListView.cellFactory = TagListCellFactory()
-        selectedTagsListView.items = tagSelectionView.selectedTagList
     }
     
     @FXML
@@ -69,10 +54,10 @@ class MainController(
     
     @FXML
     fun search() {
-        thumbnailsView.images = if (tagSelectionView.isNotSelected()) {
+        thumbnailsView.images = if (tagSelectController.isNotSelected()) {
             storedImageRepository.findNotTaggedImage()
         } else {
-            storedImageRepository.find(tagSelectionView.selectedTagList)
+            storedImageRepository.find(tagSelectController.selectedTagList)
         }
     }
     
@@ -81,22 +66,6 @@ class MainController(
         thumbnailsView.selectedThumbnail?.apply {
             singleImageViewerWindow.open(imageFile, thumbnailsView.images)
         }
-    }
-    
-    @FXML
-    fun reloadTags() {
-        tagSelectionView.reload()
-        thumbnailsView.clear()
-    }
-    
-    @FXML
-    fun clearTagSelect() {
-        tagSelectionView.clearSelect()
-    }
-    
-    @FXML
-    fun deselectTag() {
-        tagSelectionView.deselectTag()
     }
     
     @FXML
